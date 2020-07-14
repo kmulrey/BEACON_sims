26ce61f80161447171ec1bdc66dd71f57c0ebba7
Katie <kmurley@gmail.com>
2020-07-14 17:37:33 +0200

    .

--- submit.sh
+++ submit.sh
@@ -3,35 +3,35 @@
 
 cd /user/kmulrey/beacon/BEACON_sims/jobs_proton
 
-qsub -t 1-15 10000_coreas_proton.q
-qsub -t 1-15 10001_coreas_proton.q
-qsub -t 1-15 10002_coreas_proton.q
-qsub -t 1-15 10003_coreas_proton.q
+qsub -t 1-15 10000_corsika_proton.q
+qsub -t 1-15 10001_corsika_proton.q
+qsub -t 1-15 10002_corsika_proton.q
+qsub -t 1-15 10003_corsika_proton.q
 
-qsub -t 1-15 20000_coreas_proton.q
-qsub -t 1-15 20001_coreas_proton.q
-qsub -t 1-15 20002_coreas_proton.q
-qsub -t 1-15 20003_coreas_proton.q
+qsub -t 1-15 20000_ccorsika_proton.q
+qsub -t 1-15 20001_corsika_proton.q
+qsub -t 1-15 20002_corsika_proton.q
+qsub -t 1-15 20003_corsika_proton.q
 
-qsub -t 1-15 30000_coreas_proton.q
-qsub -t 1-15 30001_coreas_proton.q
-qsub -t 1-15 30002_coreas_proton.q
-qsub -t 1-15 30003_coreas_proton.q
+qsub -t 1-15 30000_corsika_proton.q
+qsub -t 1-15 30001_corsika_proton.q
+qsub -t 1-15 30002_corsika_proton.q
+qsub -t 1-15 30003_corsika_proton.q
 
 cd /user/kmulrey/beacon/BEACON_sims/jobs_iron
 
-qsub -t 1-15 10000_coreas_iron.q
-qsub -t 1-15 10001_coreas_iron.q
-qsub -t 1-15 10002_coreas_iron.q
-qsub -t 1-15 10003_coreas_iron.q
+qsub -t 1-15 10000_corsika_iron.q
+qsub -t 1-15 10001_corsika_iron.q
+qsub -t 1-15 10002_corsika_iron.q
+qsub -t 1-15 10003_corsika_iron.q
 
-qsub -t 1-15 20000_coreas_iron.q
-qsub -t 1-15 20001_coreas_iron.q
-qsub -t 1-15 20002_coreas_iron.q
-qsub -t 1-15 20003_coreas_iron.q
+qsub -t 1-15 20000_corsika_iron.q
+qsub -t 1-15 20001_corsika_iron.q
+qsub -t 1-15 20002_corsika_iron.q
+qsub -t 1-15 20003_corsika_iron.q
 
-qsub -t 1-15 30000_coreas_iron.q
-qsub -t 1-15 30001_coreas_iron.q
-qsub -t 1-15 30002_coreas_iron.q
-qsub -t 1-15 30003_coreas_iron.q
+qsub -t 1-15 30000_corsika_iron.q
+qsub -t 1-15 30001_corsika_iron.q
+qsub -t 1-15 30002_corsika_iron.q
+qsub -t 1-15 30003_corsika_iron.q
 
